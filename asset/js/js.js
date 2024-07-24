55ad49120a9a014bef2a9b50f11e03086f9ba759
Shadanmoosa <shadanmoosa@gmail.com>
2024-07-24 16:58:31 +0530

    project

---
+++ asset/js/js.js
@@ -0,0 +1,7 @@
+document.getElementById('clickableImage').addEventListener('click', function() {
+    document.getElementById('colorDiv').style.zIndex = '5'; 
+});
+
+document.getElementById('clicktwo').addEventListener('click', function() {
+    document.getElementById('colorDiv').style.zIndex = '-2'; 
+});
