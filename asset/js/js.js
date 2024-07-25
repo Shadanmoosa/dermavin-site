f5a525b7b2927a343c7cf241c1965d68a644eb4a
Shadanmoosa <shadanmoosa@gmail.com>
2024-07-25 14:49:52 +0530

    mhn

--- asset/js/js.js
+++ asset/js/js.js
@@ -5,3 +5,53 @@ document.getElementById('clickableImage').addEventListener('click', function() {
 document.getElementById('clicktwo').addEventListener('click', function() {
     document.getElementById('colorDiv').style.zIndex = '-2'; 
 });
+
+function myFunction() {
+    var element = document.body;
+    element.dataset.bsTheme =
+      element.dataset.bsTheme == "dark" ? "light" : "dark";
+      a=document.getElementById('main').style.backgroundColor;
+      if (a=="white"){
+          document.getElementById('main').style.backgroundColor="rgb(33, 37, 41)";
+      }
+      else
+      {
+          document.getElementById('main').style.backgroundColor="white";
+      }
+      a=document.getElementById('side').style.backgroundColor;
+      if (a=="white"){
+          document.getElementById('side').style.backgroundColor="rgb(33, 37, 41)";
+      }
+      else
+      {
+          document.getElementById('side').style.backgroundColor="white";
+      }
+      a=document.getElementById('side1').style.backgroundColor;
+      if (a=="white"){
+          document.getElementById('side1').style.backgroundColor="rgb(33, 37, 41)";
+      }
+      else
+      {
+          document.getElementById('side1').style.backgroundColor="white";
+      }
+      a=document.getElementById('side2').style.backgroundColor;
+      if (a=="white"){
+          document.getElementById('side2').style.backgroundColor="rgb(33, 37, 41)";
+      }
+      else
+      {
+          document.getElementById('side2').style.backgroundColor="white";
+      }
+      a=document.getElementById('side3').style.backgroundColor;
+      if (a=="ghostwhite"){
+          document.getElementById('side3').style.backgroundColor="rgb(33, 37, 41)";
+      }
+      else
+      {
+          document.getElementById('side3').style.backgroundColor="ghostwhite";
+      }
+
+
+    
+    
+  }
